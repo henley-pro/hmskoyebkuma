a52a331ae6a6b6d6eba68ce6e4840d5eea79fcb6
henley-pro <henley-pro@saramandif.com>
2025-09-03 10:57:57 +0200

    Update run.sh
    Correct verbose mode on litestream

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
+echo "****************************************"
 echo "from blackblaze (if it exists)..."
 echo "trying to restore the database (if it exists)..."
-litestream restore -v -if-replica-exists /app/data/kuma.db
+litestream restore -if-replica-exists /app/data/kuma.db
 
 echo "starting replication and the application..."
 litestream replicate
