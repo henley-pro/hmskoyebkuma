f102e833bf7db42ee51e82cf272c836ab1492f8a
henley-pro <henley-pro@saramandif.com>
2025-10-23 11:13:03 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 echo "****************************************"
 #echo "from blackblaze (if it exists)..."
-#echo "trying to restore the database (if it exists)..."
-#litestream restore -if-replica-exists /app/data/kuma.db
+echo "trying to restore the database (if it exists)..."
+litestream restore -if-replica-exists /app/data/kuma.db
 
-#echo "starting replication and the application..."
-#litestream replicate
-
+echo "starting replication and the application..."
+litestream replicate
